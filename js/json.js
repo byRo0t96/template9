e084e76dbf7b9c67208e328a92717f3db46cfc76
Yasser BDJ (Ro0t-96) <by.root96@gmail.com>
2020-08-21 17:12:08 +0000

    0.20.10

--- js/json.js
+++ js/json.js
@@ -13,18 +13,31 @@ function json(){
 					include(json_obt.head[i].icon,"ico");
 					
 					/**/
+					var logo_id = document.getElementById('logo_id');
+					var logo_code ='<!-- Main page logo -->'+
+					'<a href="" style="background-image: '+
+					"url('"+json_obt.head[i].logo+"')"+
+					';background-repeat: no-repeat;">'+json_obt.head[i].logo_url_name+'</a>'+
+					'<!-- Main page headline -->'+
+					'<span class="ng-binding">'+json_obt.head[i].logo_desc+'</span>';
+					logo_id.innerHTML=logo_code;
+					
+					
+					/**/
+					var links_id = document.getElementById('links');
 					var links="";
 					for (l = 0; l < json_obt.head[i].links_url.length; l++){
 						if (l==0){var active='active';}else{var active='';}
 						links=links+'<li '+active+'><a href="'+json_obt.head[i].links_url[l].url+'"><span class="'+json_obt.head[i].links_url[l].icon+' "></span> '+json_obt.head[i].links_url[l].name+'</a></li>';
 					}
+					links_id.innerHTML='<ul class="nav navbar-nav">'+links+'</ul>';
 					
 					/**/
-					var footer="000000000000000";
-					
+					var footer_id=document.getElementById('footer');
+					footer_id.innerHTML='Copyright &copy; '+new Date().getFullYear()+' '+json_obt.head[i].name+' All rights reserved.<br>E-mail : '+json_obt.head[i].mail+'<br>Version: '+json_obt.head[i].version;
 					
 					/**/
-					var body_code = document.getElementById("body");
+					/*var body_code = document.getElementById("body");
 					var replaceArray = [
 					'{logo}',
 					'{logo_dec}',
@@ -44,7 +57,7 @@ function json(){
 					for (var k = replaceArray.length - 1; k >= 0; k--) {
 						finalAns = finalAns.replace(replaceArray[k], replaceArrayValue[k]);
 						}
-					body_code.innerHTML=finalAns;
+					body_code.innerHTML=finalAns;*/
 				}            
 	}
 	};
